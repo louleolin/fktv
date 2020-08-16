642b0cd001ba27e8a372bfff9493bfd3c7d1d95c
louleo <louleolin@gmail.com>
2020-08-17 08:44:18 +1000

    Inject a fixed clock into videojs

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", function() {
 
-    var s = document.createElement('script');
-    // TODO: add "script.js" to web_accessible_resources in manifest.json
-    s.src = chrome.extension.getURL('inject.js');
-    s.onload = function () {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-
+    setTimeout(function(){
+        var s = document.createElement('script');
+        // TODO: add "script.js" to web_accessible_resources in manifest.json
+        s.src = chrome.extension.getURL('inject.js');
+        s.onload = function () {
+            this.remove();
+        };
+        (document.head || document.documentElement).appendChild(s);
+    },5000);
 });
--- inject.js
+++ inject.js
@@ -1,16 +1,35 @@
-(function(open) {
-     XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-         if(url.includes('.mp4')){
-            url = url.substr(0,-10);
-          }
-          if (url.includes('Customize') && url.includes('list?')) {
-              console.log(url);
-              url = "https://louleo.github.io/ps/json/ads.json";
-          }
-         if (url.includes('Statistics') && url.includes('ads_show?')) {
-             console.log(url);
-             url = "https://louleo.github.io/ps/json/code.json";
-         }
-         open.apply(this, arguments);
-     };
- })(XMLHttpRequest.prototype.open);
+// (function(open) {
+//      // XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+//      //     if(url.includes('.mp4')){
+//      //        url = url.substr(0,-10);
+//      //      }
+//      //      if (url.includes('Customize') && url.includes('list?')) {
+//      //          console.log(url);
+//      //          url = "https://louleo.github.io/ps/json/ads.json";
+//      //      }
+//      //     if (url.includes('Statistics') && url.includes('ads_show?')) {
+//      //         console.log(url);
+//      //         url = "https://louleo.github.io/ps/json/code.json";
+//      //     }
+//      //     open.apply(this, arguments);
+//      // };
+//  })(XMLHttpRequest.prototype.open);
+if (document.querySelector('.video-js')){
+ var clock = document.createElement('div');
+ clock.id = "leo_clock";
+ var leoclockdate = new Date();
+ clock.innerHTML = leoclockdate.getHours()+':'+leoclockdate.getMinutes();
+ document.querySelector('.video-js').append(clock);
+ setInterval(function(){
+  var leoclockdate = new Date();
+  document.querySelector('#leo_clock').innerHTML=leoclockdate.getHours()+':'+(leoclockdate.getMinutes()>10?leoclockdate.getMinutes():('0'+leoclockdate.getMinutes()));
+ },60000);
+ document.querySelector('#vjs-fullscreen-button').addEventListener('click',function(){
+  if(document.querySelector('.video-js').className.indexOf('vjs-fullscreen') > -1){
+   document.querySelector('#leo_clock').setAttribute('style','position:fixed;z-index:99999;top:10px;right:10px;padding:10px 25px;font-size:25px;background-color:rgba(0,0,0,0.2);display:block!important;color:white;border-radius:50px;');
+  }else{
+   document.querySelector('#leo_clock').setAttribute('style','');
+  }
+ });
+}
+
