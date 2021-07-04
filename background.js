5505f8b723a8587994ffaba6d5487e8eae639fa3
louleolin <liu.louleo@gmail.com>
2021-07-04 18:55:55 +1000

    Improve performance for blocking ads on iyingshi

--- background.js
+++ background.js
@@ -6,7 +6,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     if(details.url.indexOf("iyingshi5.tv/nplayer/admin/api.php") != -1){
         return {redirectUrl: "https://run.mocky.io/v3/1bdb01d7-f643-4673-bd0d-066c5785f3a2"};
     }
-    return {cancel: (details.url.indexOf("audio/advert") != -1)};
   },
   { urls: ["*://*.iyingshi5.tv/*", "*://*.guazitv.tv/*","*://guazitv.tv/*","*://*.guazitv8.com/audio/advert/*", "*://www.iyingshi9.tv/*", "*://api.localau.vip/*"] },
   ["blocking"]
