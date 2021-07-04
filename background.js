aee81237718efe1a30dc4ac7f34158ec038e09ee
louleolin <liu.louleo@gmail.com>
2021-07-04 16:38:17 +1000

    Update for iyingshi5.tv

--- background.js
+++ background.js
@@ -1,25 +1,13 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    /* comment out for kantv
-      if((details.url.indexOf("list") != -1)&&(details.url.indexOf("Customize") !=-1)) {
-          return {redirectUrl: "https://www.kantv6.com/index.php/Customize/list?params=%7B%22type%22%3A%7B%22logo%22%3A1%7D%7D"};
-      }
-      if((details.url.indexOf("ads_show") != -1)&&(details.url.indexOf("Statistics") !=-1)){
-          return {redirectUrl: "https://www.kantv6.com/index.php/Customize/list?params=%7B%22type%22%3A%7B%22logo%22%3A1%7D%7D"};
-      }
-    return {cancel:( (details.url.indexOf("list") != -1)&&(details.url.indexOf("Customize") !=-1) )||((details.url.indexOf("ads_show") != -1)&&(details.url.indexOf("Statistics") !=-1))};
-    */
-    // if(details.url.indexOf("audio/advert") != -1){
-    //   return {redirectUrl: "https://www.kantv6.com/index.php/Customize/list?params=%7B%22type%22%3A%7B%22logo%22%3A1%7D%7D"};
-    // }
-    if(details.url.indexOf("addon/wechat/api/checkLogin2") != -1 && details.url.indexOf("iyingshi") != -1){
-      //return {cancel: true};
-    }
     if(details.url.indexOf("api.localau.vip/ads") != -1){
       return {cancel: true};
     }
+    if(details.url.indexOf("iyingshi5.tv/nplayer/admin/api.php") != -1){
+        return {redirectUrl: "https://run.mocky.io/v3/1bdb01d7-f643-4673-bd0d-066c5785f3a2"};
+    }
     return {cancel: (details.url.indexOf("audio/advert") != -1)};
   },
-  { urls: ["*://*.kantv6.com/*", "*://*.guazitv.tv/*","*://guazitv.tv/*","*://*.guazitv8.com/audio/advert/*", "*://www.iyingshi9.tv/*", "*://api.localau.vip/*"] },
+  { urls: ["*://*.iyingshi5.tv/*", "*://*.guazitv.tv/*","*://guazitv.tv/*","*://*.guazitv8.com/audio/advert/*", "*://www.iyingshi9.tv/*", "*://api.localau.vip/*"] },
   ["blocking"]
 );
--- inject.js
+++ inject.js
@@ -1,19 +1,3 @@
-// (function(open) {
-//      // XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-//      //     if(url.includes('.mp4')){
-//      //        url = url.substr(0,-10);
-//      //      }
-//      //      if (url.includes('Customize') && url.includes('list?')) {
-//      //          console.log(url);
-//      //          url = "https://louleo.github.io/ps/json/ads.json";
-//      //      }
-//      //     if (url.includes('Statistics') && url.includes('ads_show?')) {
-//      //         console.log(url);
-//      //         url = "https://louleo.github.io/ps/json/code.json";
-//      //     }
-//      //     open.apply(this, arguments);
-//      // };
-//  })(XMLHttpRequest.prototype.open);
 if (document.querySelector('.video-js')){
  var clock = document.createElement('div');
  clock.id = "leo_clock";
